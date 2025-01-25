568c5e28354b42a9427c28d7d63b1a933622a067
Joe <josephaddunne@gmail.com>
2025-01-25 20:16:40 +0000

    Final result printed, and example updated

--- agent.py
+++ agent.py
@@ -12,13 +12,13 @@ llm=ChatOpenAI(base_url='https://api.deepseek.com/v1', model='deepseek-chat')
 
 async def RunAgent(agent: Agent):
     result = await agent.run()
-    print(result)
+    print(result.final_result())
 
 
 def main():
 
     parser = argparse.ArgumentParser(description="Run agent with custom task")
-    parser.add_argument("--task", type=str, default="Compare O4 to DeepSeek, and save as a PDF. Only use a text editor online that does not require signing in", help="Specify the task for the agent")
+    parser.add_argument("--task", type=str, default="Search what is the tallest ferris wheel in the world", help="Specify the task for the agent")
     args = parser.parse_args()
 
     agent = Agent(
