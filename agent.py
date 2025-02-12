23022bfa86e15bc66d1c139d61a300695aea64a8
zapper_95 <josephaddunne@gmail.com>
2025-02-12 10:29:55 +0000

    close any open chrome tabs and better formatted

--- agent.py
+++ agent.py
@@ -5,47 +5,63 @@ from dotenv import load_dotenv
 import asyncio
 import argparse
 import os
+import subprocess
 
 load_dotenv()
 
-api_key = os.getenv('OPENAI_API_KEY', '')
+api_key = os.getenv("OPENAI_API_KEY", "")
 if not api_key:
-	raise ValueError('OPENAI_API_KEY is not set')
+    raise ValueError("OPENAI_API_KEY is not set")
 
-# Configure the browser to connect to your Chrome instance
+
 browser = Browser(
     config=BrowserConfig(
-        # Specify the path to your Chrome executable
-        chrome_instance_path='/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',  # macOS path
+        chrome_instance_path="/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",  # macOS path
     )
 )
 
-# Initialize the model
-llm=ChatOpenAI(
-    model='gpt-4o-mini',
+llm = ChatOpenAI(
+    model="gpt-4o-mini",
     api_key=SecretStr(api_key),
 )
+
 
 async def RunAgent(agent: Agent):
     result = await agent.run()
     print(result.final_result())
 
 
-def main():
-
+def GetArgs():
     parser = argparse.ArgumentParser(description="Run agent with custom task")
-    parser.add_argument("--task", type=str, default="Search what is the tallest ferris wheel in the world and write to a google doc", help="Specify the task for the agent")
-    args = parser.parse_args()
-
-    agent = Agent(
-    task=args.task,
-    llm=llm,
-    use_vision=False,
-    max_failures=5,
-    browser=browser,
+    parser.add_argument(
+        "--task",
+        type=str,
+        default="Search what is the tallest ferris wheel in the world and write to a google doc",
+        help="Specify the task for the agent",
     )
+    args = parser.parse_args()
+    return args
 
+
+def CreateAgent(args):
+    agent = Agent(
+        task=args.task,
+        llm=llm,
+        use_vision=False,
+        max_failures=5,
+        browser=browser,
+    )
+    return agent
+
+
+def main():
+    # forcefully closes any existing chrome tabs
+    subprocess.call("pkill -9 -f 'Google Chrome'", shell=True)
+
+    args = GetArgs()
+    agent = CreateAgent(args)
     asyncio.run(RunAgent(agent))
+
 
 if __name__ == "__main__":
     main()
